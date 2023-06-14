7bd8a59564e6a8ab4bae757603847c6c7100c4ad
RAO.sh <daulatrao.patil@walchandsangli.ac.in>
2023-06-14 13:26:27 +0530

    Add: Scripts

---
+++ background.js
@@ -0,0 +1,6 @@
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.message === "activate_icon") chrome.pageAction.show(sender.tab.id);
+    else if (request.message === "inactivate_icon") chrome.pageAction.hide(sender.tab.id);
+    
+    console.log(sender.tab.id);
+});
