7bb5b122ee3bdbe33ea41af2ee37ee53f1ead326
Swarada Bhosale <swaradapb113@gmail.com>
2023-06-17 19:56:51 +0530

    Change in styling and a bit of functioning

--- popup.js
+++ popup.js
@@ -1,68 +1,89 @@
-let rangeText = document.getElementById("rangeText");
-let slider = document.getElementById("playbackSlider");
-let clearButton = document.getElementById("clearButton");
+let rangeText = document.getElementById('rangeText')
+let slider = document.getElementById('playbackSlider')
+let clearButton = document.getElementById('clearButton')
+let slideValue = document.querySelector('span')
 
-const defaultSpeed = 1.0;
-const lowerLimit = 0;
-const higherLimit = 16;
+const defaultSpeed = 1.0
+const lowerLimit = 0
+const higherLimit = 16
 
-function setSpeed(speed){
-   slider.value = speed;
+//setting the slider value to rangeText value
+const changeSpeed = () => {
+  let value = parseFloat(rangeText.value)
+  value = Math.round(value * 10) / 10
+  if (value < lowerLimit) value = lowerLimit
+  if (value > higherLimit) value = higherLimit
+  setSpeed(value)
+}
+rangeText.addEventListener('keydown', (e) => {
+  if (e.key !== 'Enter') return
+  changeSpeed()
+  updateSlider()
+})
 
-   let speedString = speed.toString();
-   if(speedString.indexOf(".") == 1) speedString += ".0";
-   
-   rangeText.value = speedString;
-   updatePlaybackSpeed(speed);
+//adding show class, setting rangeText value to slider value
+const updateSlider = () => {
+  let value = slider.value
+  slideValue.textContent = value
+  slideValue.style.left =
+    ((value - slider.min) / (slider.max - slider.min)) * 100 + '%'
+  slideValue.classList.add('show')
+  rangeText.value = value
+  changeSpeed()
+}
+slider.addEventListener('input', updateSlider)
+slider.addEventListener('click', updateSlider)
+slider.addEventListener('blur', () => {
+  slideValue.classList.remove('show')
+})
+
+//setting speed
+function setSpeed(speed) {
+  slider.value = speed
+  let speedString = speed.toString()
+  if (speedString.indexOf('.') === 1) speedString += '.0'
+  rangeText.value = speed
+  updatePlaybackSpeed(speed)
+  slideValue.classList.add('show')
 }
 
-function getPlaybackSpeed(){
-   chrome.tabs.query({
-      "active" : true,
-      "status" : "complete"
-   }, (tabs)=>{
-      if(tabs.length == 0){
-         setTimeout(getPlaybackSpeed, 100);
-         return;
+function getPlaybackSpeed() {
+  chrome.tabs.query(
+    {
+      active: true,
+      status: 'complete',
+    },
+    (tabs) => {
+      if (tabs.length == 0) {
+        setTimeout(getPlaybackSpeed, 100)
+        return
       }
 
-      let tab = tabs[0];
-      chrome.tabs.sendMessage(tab.id, {"message" : "get playback speed"});
-   });
+      let tab = tabs[0]
+      chrome.tabs.sendMessage(tab.id, { message: 'get playback speed' })
+    }
+  )
 }
 
-getPlaybackSpeed();
+getPlaybackSpeed()
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse)=>{
-   if(request.message == "current speed"){
-      let speed = request.speed;
-      setSpeed(request.speed);
-   }
-});
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message == 'current speed') {
+    let speed = request.speed
+    setSpeed(request.speed)
+  }
+})
 
-clearButton.addEventListener("click", ()=>{
-   setSpeed(defaultSpeed);
-});
-
-rangeText.addEventListener("keydown", (e)=>{
-   if(e.key != "Enter") return;
-
-   let value = parseFloat(rangeText.value);
-   value = Math.round(value * 10) / 10;
-   
-   if(value < lowerLimit) value = lowerLimit;
-   if(value > higherLimit) value = higherLimit;
-   
-   setSpeed(value);
-});
-
-slider.addEventListener("input", (e)=>{
-   let speed = slider.value;
-   setSpeed(speed);
-});
-
-function updatePlaybackSpeed(speed){
-   chrome.tabs.getSelected(undefined, (tab)=>{
-      chrome.tabs.sendMessage(tab.id, {"message" : "set playback speed", "speed" : speed});
-   });
+function updatePlaybackSpeed(speed) {
+  chrome.tabs.getSelected(undefined, (tab) => {
+    chrome.tabs.sendMessage(tab.id, {
+      message: 'set playback speed',
+      speed: speed,
+    })
+  })
 }
+
+clearButton.addEventListener('click', () => {
+  setSpeed(defaultSpeed)
+  updateSlider()
+})
